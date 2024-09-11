d3dcf7731db5f47fa132e5d56576be2026cc19a1
ik <jeongik11029@naver.com>
2024-09-12 00:21:44 +0900

    level 2-1. 테스트 연습 - 1

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
@@ -23,6 +24,6 @@ class PasswordEncoderTest {
         boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
 
         // then
-        assertTrue(matches);
+        assertThat(matches).isFalse();
     }
 }
